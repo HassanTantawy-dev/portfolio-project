f4251b9e75ae1b3d710f5f4bb63c936e8e894e87
HassanTantawy-dev <hasantantawy@icloud.com>
2026-01-25 14:39:20 +0200

    edite

--- js/main1.js
+++ js/main1.js
@@ -1,22 +1,34 @@
+const toggleBtn = document.getElementById("darkModeToggle");
 
-    const toggleBtn = document.getElementById("darkModeToggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 
-    toggleBtn.addEventListener("click", () => {
-        document.body.classList.toggle("dark-mode");
+  // تغيير الأيقونة
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.innerText = "☀️";
+    localStorage.setItem("theme", "dark");
+  } else {
+    toggleBtn.innerText = "🌙";
+    localStorage.setItem("theme", "light");
+  }
+});
 
-        // تغيير الأيقونة
-        if (document.body.classList.contains("dark-mode")) {
-            toggleBtn.innerText = "☀️";
-            localStorage.setItem("theme", "dark");
-        } else {
-            toggleBtn.innerText = "🌙";
-            localStorage.setItem("theme", "light");
-        }
+// حفظ الاختيار
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggleBtn.innerText = "☀️";
+}
+
+document.querySelector("#massege").addEventListener("click", function (event) {
+  if (!document.querySelector("form").checkValidity()) {
+    return;
+  } else {
+    Swal.fire({
+      icon: "success",
+      title: "A message has been sent successfully",
+      showConfirmButton: false,
+      timer: 1400,
     });
-
-    // حفظ الاختيار
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark-mode");
-        toggleBtn.innerText = "☀️";
-    }
-
+  }
+  event.preventDefault();
+});
